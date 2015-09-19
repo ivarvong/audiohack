37e96726fbc127bed500f7e66051320f3f6a0d5d
Ivar Vong <ivar@ivarvong.com>
2015-09-19 18:26:58 -0400

    fb meta

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -1,11 +1,16 @@
 class Clip < ActiveRecord::Base
 	belongs_to :episode
 	belongs_to :user
-	
+
 	def to_param
 		#{}"#{id}-#{name.parameterize}"
 		["#{id}", "#{user.try(:name).parameterize}", "#{episode.try(:name).parameterize}"].join("-")
 	rescue
 		"#{id}"
 	end
+
+	def get_image_url
+		self.image_url || self.episode.try(:image_url)
+	end
+
 end
