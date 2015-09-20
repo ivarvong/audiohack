67211c3696470e88f0317d7bf6656b599c383ef3
Ivar Vong <ivar@ivarvong.com>
2015-09-20 10:46:49 -0400

    s3 uploader helper

---
+++ app/lib/s3_uploader.rb
@@ -0,0 +1,23 @@
+class S3Uploader
+	def self.perform(options={})
+		# kword args: access_key, access_secret, bucket, key, contents
+		# returns public url
+
+		url = AWS::S3.new(
+			access_key_id: options[:access_key],
+			secret_access_key: options[:access_secret]
+		)
+		.buckets[options[:bucket]]
+		.objects[options[:key]]
+		.write(
+		 	options[:contents],
+		 	acl: options[:acl] || :public_read,
+		 	content_type: options[:content_type] || '',
+		).public_url.to_s
+
+		puts "S3Uploaded: #{url}"
+
+		return url
+
+	end
+end
